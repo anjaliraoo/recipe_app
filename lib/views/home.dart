c6d5b0bd6746b974acfbc359f3a5d778029187d4
anjaliraoo <anjaliraoo1010@gmail.com>
2023-03-22 20:48:00 +0530

    Updated Screens

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -53,96 +53,110 @@ class _HomeState extends State<Home> {
                 LinearGradient(colors: [Color(0xff213A50), Color(0xff071930)]),
           ),
         ),
-        Container(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-                vertical: Platform.isIOS ? 60 : 30, horizontal: 30),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: kIsWeb
-                      ? MainAxisAlignment.start
-                      : MainAxisAlignment.center,
-                  children: const [
-                    Text(
-                      'AppGuy',
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white),
-                    ),
-                    Text(
-                      'Recipes',
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.blue),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  "What will you cook today?",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                const SizedBox(
-                  height: 8,
-                ),
-                const Text(
-                  "Just Enter Ingredients you have and we will show the best recipe for you",
-                  style: TextStyle(fontSize: 15, color: Colors.white),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: textEditingController,
-                          decoration: InputDecoration(
-                            hintText: "Enter Ingredients",
-                            hintStyle: TextStyle(
-                              fontSize: 18,
-                              color: Colors.white.withOpacity(0.3),
-                            ),
-                          ),
-                          style: const TextStyle(
+        SingleChildScrollView(
+          child: Container(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: Platform.isIOS ? 60 : 30, horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: kIsWeb
+                        ? MainAxisAlignment.start
+                        : MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Recipes',
+                        style: TextStyle(
                             fontSize: 18,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 16,
-                      ),
-                      InkWell(
-                        onTap: () {
-                          if (textEditingController.text.isNotEmpty) {
-                            getRecipes(textEditingController.text);
-                            print("jyst");
-                          } else {
-                            print("don;t");
-                          }
-                        },
-                        child: Container(
-                          child: const Icon(
-                            Icons.search,
-                            color: Colors.white,
-                          ),
-                        ),
+                            fontWeight: FontWeight.w500,
+                            color: Colors.blue),
                       )
                     ],
                   ),
-                ),
-
-                
-              ],
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text(
+                    "What will you cook today?",
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const Text(
+                    "Just Enter Ingredients you have and we will show the best recipe for you",
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            controller: textEditingController,
+                            decoration: InputDecoration(
+                              hintText: "Enter Ingredients",
+                              hintStyle: TextStyle(
+                                fontSize: 18,
+                                color: Colors.white.withOpacity(0.3),
+                              ),
+                            ),
+                            style: const TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            if (textEditingController.text.isNotEmpty) {
+                              getRecipes(textEditingController.text);
+                              print("jyst");
+                            } else {
+                              print("don;t");
+                            }
+                          },
+                          child: Container(
+                            child: const Icon(
+                              Icons.search,
+                              color: Colors.white,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: GridView(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.vertical,
+                      physics: ClampingScrollPhysics(),
+                      gridDelegate:
+                          const SliverGridDelegateWithMaxCrossAxisExtent(
+                        maxCrossAxisExtent: 200,
+                        mainAxisSpacing: 10.0,
+                      ),
+                      children: List.generate(recipes.length, (index) {
+                        return GridTile(
+                            child: RecipieTile(
+                          title: recipes[index].label,
+                          desc: recipes[index].source,
+                          imgUrl: recipes[index].image,
+                          url: recipes[index].url,
+                        ));
+                      }),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
@@ -154,7 +168,8 @@ class _HomeState extends State<Home> {
 class RecipieTile extends StatefulWidget {
   final String title, desc, imgUrl, url;
 
-  const RecipieTile({this.title='', this.desc='', this.imgUrl='', this.url=''});
+  const RecipieTile(
+      {this.title = '', this.desc = '', this.imgUrl = '', this.url = ''});
 
   @override
   _RecipieTileState createState() => _RecipieTileState();
--- lib/views/recipe_view.dart
+++ lib/views/recipe_view.dart
@@ -1,49 +1,71 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class RecipeView extends StatefulWidget {
   //const RecipeView({super.key});
 
   final String postUrl;
-  RecipeView({this.postUrl=""});
+  RecipeView({this.postUrl = ""});
 
   @override
   State<RecipeView> createState() => _RecipeViewState();
 }
 
 class _RecipeViewState extends State<RecipeView> {
-  
+  String finalUrl = "";
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    if (widget.postUrl.contains("http://")) {
+      finalUrl = widget.postUrl.replaceAll("http://", "https://");
+    } else {
+      finalUrl = widget.postUrl;
+    }
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-        Container(
-          width: MediaQuery.of(context).size.width,
-          child: Row (
-                mainAxisAlignment: kIsWeb
-                        ? MainAxisAlignment.start
-                        : MainAxisAlignment.center,
-                    children: const [
-                      Text(
-                        'AppGuy',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white),
-                      ),
-                      Text(
-                        'Recipes',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.blue),
-                      )
-                    ],
-                  ),
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.black54,
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                      colors: [Colors.white30, Colors.white],
+                      begin: FractionalOffset.centerRight,
+                      end: FractionalOffset.centerLeft)),
+              padding: EdgeInsets.only(
+                  top: Platform.isIOS ? 60 : 30,
+                  right: 24,
+                  left: 24,
+                  bottom: 16),
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment:
+                    kIsWeb ? MainAxisAlignment.start : MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'Recipes',
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.blue),
+                  )
+                ],
+              ),
+            ),
+          ],
         ),
-        ],
       ),
     );
   }
